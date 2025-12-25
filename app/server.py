6093a328f79eef991da3f3fae7758e7b66f5d2f4
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-25 10:30:05 +0700

    feat:get plant by id

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 import json
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from app.model import plant
 from app.model.plant import Plant
@@ -18,9 +18,21 @@ def read_root():
     }
 
 @app.get("/plants",response_model=list[Plant],summary="Get All Plants")
-def get_all_plants()->list[Plant]:
+async def get_all_plants()->list[Plant]:
     
     with open("../app/plant_db.json","r") as file:
         plant = json.load(file)
     result = plant
     return result
+
+@app.get("/plant/{id}" , response_model=Plant , summary="Get Plant by Id")
+def get_plant_by_id(id:int)->Plant:
+        
+    with open("../app/plant_db.json","r") as file:
+        plant = json.load(file)
+        
+    for i in plant:
+        if i["id"] == id:
+            return Plant(**i)
+    
+    raise HTTPException(status_code=404, detail={"error": "Animal not found"})
