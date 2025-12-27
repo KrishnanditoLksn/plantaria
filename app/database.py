5cf61d0366766f25008c5f076c4a4503f0dba98b
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-27 12:33:41 +0700

    feat:configure database class

---
+++ app/database.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DB_HOST = os.getenv("MYSQL_HOST")
+DB_PORT = os.getenv("MYSQL_PORT")
+DB_NAME = os.getenv("MYSQL_DB")
+DB_USER = os.getenv("MYSQL_USER")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True  # opsional, untuk debug SQL
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
