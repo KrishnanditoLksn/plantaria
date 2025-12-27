1586955708710868675d337d3e230109ca88d249
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-27 12:34:04 +0700

    feat:set plant model entity

--- app/model/plant.py
+++ app/model/plant.py
@@ -2,29 +2,43 @@ import json
 from typing import Optional
 from fastapi import Body,FastAPI,HTTPException,Query
 from pydantic import BaseModel,Field
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String
+from app.database import Base
 
 app = FastAPI(
-    title="Plants API", description="Plants FastAPI Example", version="0.1.0"
+    title="Plants API", 
+    description="Plants FastAPI Example", 
+    version="0.1.0"
 )
 
-class Plant(BaseModel):
-    name:str = Field(
-        description="Plant Name"
+class Plant(Base):
+    __tablename__ = "plant"
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True
     )
-    species:str=Field(
-        description="Plant Species"
+    
+    species : Mapped[str] = mapped_column(
+        String(50), 
+        unique=True
     )
-    age:int = Field(
-        description="Plant Age"
+    
+    age = Column(
+        Integer,
+        index=True
     )
-    habitat:str=Field(
-        description="Habitat Plant"
+    
+    habitat = Column(
+        Integer,
+        index=True
     )
 
-
-class UpdatePlant(BaseModel):
-    name: Optional[str] = Field(None, description="Plant name")
-    species: Optional[str] = Field(None, description="Plant species")
-    age: Optional[int] = Field(None, description="Plant age")
-    habitat: Optional[str] = Field(None, description="Plant habitat")
-    diet: Optional[str] = Field(None, description="Plant diet")
+# class UpdatePlant(BaseModel):
+#     name: Optional[str] = Field(None, description="Plant name")
+#     species: Optional[str] = Field(None, description="Plant species")
+#     age: Optional[int] = Field(None, description="Plant age")
+#     habitat: Optional[str] = Field(None, description="Plant habitat")
+#     diet: Optional[str] = Field(None, description="Plant diet")
