407801d33170d815c9debdf6c18dcfe4e56b46b7
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-28 14:03:43 +0700

    feat:add plant service class

---
+++ app/service/plant_service.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from sqlmodel import Session
+
+from app.model.plant import Plant
+from app.schemas.request.plant_post_request import PlantRequest
+from app.db.database import get_db
+
+
+def add_plant(plant:PlantRequest,db:Session = Depends(get_db)):
+    plant = Plant(
+                species = plant.species,
+                age = plant.age,
+                habitat = plant.habitat
+            )
+    db.add(plant)
+    db.commit()
