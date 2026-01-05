53021cab5a6df3d02042e1f26fc09364461d1597
KrishnanditoLksn <krislaksana123@gmail.com>
2026-01-05 21:42:31 +0700

    feat:get plant api function

--- app/api/v1/plant_api.py
+++ app/api/v1/plant_api.py
@@ -6,12 +6,14 @@ from sqlmodel import Session
 from app.model.plant import Plant
 from app.schemas.request.plant_post_request import PlantRequest
 from app.db.database import get_db
-from app.service.plant_service import add_plant, get_plant_id
+from app.service.plant_service import add_plant, get_all_plants, get_plant_id
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/api/v1'
+)
 
-@router.post("/add")
+@router.post("/plant/add")
 def post_plant(plant:PlantRequest , db:Session = Depends(get_db)):
         try:
             is_present = db.scalars(
@@ -43,21 +45,31 @@ def post_plant(plant:PlantRequest , db:Session = Depends(get_db)):
                 }
         )
 
-@router.get("/{id}")
+@router.get("/plant/{id}")
 def get_plant_by_id(id:int , db:Session = Depends(get_db)):
     try:
         plantFound = get_plant_id(
             id,
             db
         )
-        
-        if not plantFound:
-            raise HTTPException(
-                status_code=404, 
-                detail="Plant not found"
-            )
         return plantFound
     except Exception as e :
+        raise HTTPException(
+                status_code=404,
+                detail={
+                    "status": "error",
+                    "error_type": type(e).__name__,
+                    "message": str(e),
+                    "traceback": traceback.format_exc()
+                }
+        )
+
+@router.get("/plants",response_model=list[PlantRequest])
+def get_plants(db:Session = Depends(get_db)):
+    try:
+        all_plants = get_all_plants(db)
+        return  all_plants
+    except Exception as e:
         raise HTTPException(
                 status_code=404,
                 detail={
